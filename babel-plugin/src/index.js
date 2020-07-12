7964a86fabdcbf6c25ef9742fc751d994d8466b1
klis87 <klisiczynski@gmail.com>
2020-07-12 15:46:12 +0200

    Added basePath, transformTypes and prefixTypes options to babel plugin

--- babel-plugin/src/index.js
+++ babel-plugin/src/index.js
@@ -1,43 +1,98 @@
 const { declare } = require('@babel/helper-plugin-utils');
 const { types: t } = require('@babel/core');
+const { snakeCase } = require('lodash');
 
-module.exports = declare(api => {
-  api.assertVersion(7);
+const maybeChangeImportName = (path, from, to) => {
+  if (path.node.imported.name === from) {
+    path.node.imported.name = to;
+    path.node.local.name = to;
+  }
+};
 
-  return {
-    visitor: {
-      ImportSpecifier(path) {
-        if (path.node.imported.name === 'createSmartAction') {
-          path.node.imported.name = 'createAction';
-          path.node.local.name = 'createAction';
-        } else if (path.node.imported.name === 'createSmartThunk') {
-          path.node.imported.name = 'createThunk';
-          path.node.local.name = 'createThunk';
-        }
+const removeExtension = filePath => {
+  const list = filePath.split('.');
+  return list.slice(0, list.length - 1).join('.');
+};
+
+const transformFilePath = (state, basePath) => {
+  if (!basePath) {
+    return `${removeExtension(state.file.opts.filename)}/`;
+  }
+
+  return `${removeExtension(state.file.opts.filename.split(basePath)[1])}/`;
+};
+
+const maybeTransformType = (type, transformTypes) => {
+  if (!transformTypes) {
+    return type;
+  }
+
+  return type
+    .split('/')
+    .map(v => snakeCase(v).toUpperCase())
+    .join('/');
+};
+
+const maybeTransformSmartAction = (
+  path,
+  state,
+  basePath,
+  transformTypes,
+  prefixTypes,
+  from,
+  to,
+) => {
+  if (
+    path.node.init &&
+    path.node.init.callee &&
+    path.node.init.callee.name === from
+  ) {
+    path.node.init.callee.name = to;
+    path.node.init.arguments = [
+      t.stringLiteral(
+        maybeTransformType(
+          `${prefixTypes ? transformFilePath(state, basePath) : ''}${
+            path.node.id.name
+          }`,
+          transformTypes,
+        ),
+      ),
+      ...path.node.init.arguments,
+    ];
+  }
+};
+
+module.exports = declare(
+  (api, { basePath = null, transformTypes = true, prefixTypes = false }) => {
+    api.assertVersion(7);
+
+    return {
+      visitor: {
+        ImportSpecifier(path) {
+          maybeChangeImportName(path, 'createSmartAction', 'createAction');
+          maybeChangeImportName(path, 'createSmartThunk', 'createThunk');
+        },
+        VariableDeclarator(path, state) {
+          maybeTransformSmartAction(
+            path,
+            state,
+            basePath,
+            transformTypes,
+            prefixTypes,
+            'createSmartAction',
+            'createAction',
+          );
+          maybeTransformSmartAction(
+            path,
+            state,
+            basePath,
+            transformTypes,
+            prefixTypes,
+            'createSmartThunk',
+            'createThunk',
+          );
+        },
       },
-      VariableDeclarator(path) {
-        if (
-          path.node.init &&
-          path.node.init.callee &&
-          path.node.init.callee.name === 'createSmartAction'
-        ) {
-          path.node.init.callee.name = 'createAction';
-          path.node.init.arguments = [
-            t.stringLiteral(path.node.id.name),
-            ...path.node.init.arguments,
-          ];
-        } else if (
-          path.node.init &&
-          path.node.init.callee &&
-          path.node.init.callee.name === 'createSmartThunk'
-        ) {
-          path.node.init.callee.name = 'createThunk';
-          path.node.init.arguments = [
-            t.stringLiteral(path.node.id.name),
-            ...path.node.init.arguments,
-          ];
-        }
-      },
-    },
-  };
-});
+    };
+  },
+);
